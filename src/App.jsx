174880d2cac9979e9c6708916cbb4dced3ead428
Fabian Leiva <123709798+fabianleiva@users.noreply.github.com>
2023-11-18 19:35:23 -0300

    import search, add props

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import MyApi from "./assets/components/MyApi";
+import Search from "./Search";
 
 function App() {
+  const [trendData, setTrendData] = useState([]);
+  const [searchData, setSearchData] = useState([]);
+  const [input, setInput] = useState("");
+
   return (
     <>
       <h1>GIF's Explorer</h1>
-      <MyApi />
+      <Search setInput={setInput} />
+      <MyApi
+        trendData={trendData}
+        setTrendData={setTrendData}
+        searchData={searchData}
+        setSearchData={setSearchData}
+        input={input}
+      />
     </>
   );
 }
