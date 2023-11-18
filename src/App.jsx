ce24faa97d195d99167fc2e3a89f3eafd3c0c8a7
Fabian Leiva <123709798+fabianleiva@users.noreply.github.com>
2023-11-18 10:47:22 -0300

    import bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import { useState, useEffect } from 'react'
-import './App.css'
-import MyApi from './assets/components/MyApi'
+import { useState, useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import MyApi from "./assets/components/MyApi";
 
 function App() {
-
   return (
     <>
-    <h1>Galeria de Gifs</h1>
-    <MyApi />
+      <h1>GIF's Explorer</h1>
+      <MyApi />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
