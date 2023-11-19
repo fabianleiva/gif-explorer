2fb0f0de4ce8079b4e3ed9938016556287e3598f
Fabian Leiva <123709798+fabianleiva@users.noreply.github.com>
2023-11-19 12:51:03 -0300

    import/add animate, adjust classNames, add footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import MyApi from "./assets/components/MyApi";
 import Search from "./assets/components/Search";
+import Footer from "./assets/components/Footer";
+import "animate.css";
 
 function App() {
   const [trendData, setTrendData] = useState([]);
@@ -12,19 +14,26 @@ function App() {
 
   return (
     <>
-      <div>
-        <img className="logoContainer" src="../public/ge-logo.png" alt="logo" />
+      <div className="container">
+        <header className="animate__animated animate__fadeInDown">
+          <img className="logo" src="../public/ge-logo.png" alt="logo" />
+          <Search setInput={setInput} setError={setError} />
+        </header>
+        <main className="content animate__animated animate__fadeInUp">
+          <div className="galleryContainer">
+            <MyApi
+              trendData={trendData}
+              setTrendData={setTrendData}
+              searchData={searchData}
+              setSearchData={setSearchData}
+              input={input}
+              error={error}
+              setError={setError}
+            />
+          </div>
+        </main>
+        <Footer />
       </div>
-      <Search setInput={setInput} setError={setError} />
-      <MyApi
-        trendData={trendData}
-        setTrendData={setTrendData}
-        searchData={searchData}
-        setSearchData={setSearchData}
-        input={input}
-        error={error}
-        setError={setError}
-      />
     </>
   );
 }
