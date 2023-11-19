725dc733feb513dcc0254dad01c7a180dfc6f98d
Fabian Leiva <123709798+fabianleiva@users.noreply.github.com>
2023-11-19 12:49:31 -0300

    create Footer.jsx

---
+++ src/assets/components/Footer.jsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+  return (
+    <>
+      <footer>
+        <p>
+          DesignedBy: <span>@fab.leiva</span>
+        </p>
+      </footer>
+    </>
+  );
+};
+export default Footer;
